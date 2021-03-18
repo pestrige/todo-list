d1e826d163c9cb5bd0c98cda0c955191ca192c74
pestrige <pestrige@gmail.com>
2021-03-18 22:33:31 +0300

    Добавил темную тему и добавление задачи по Enter

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,11 @@
-const todoList = document.querySelector('.todo-list__list');
-const newTaskContainer = document.querySelector('.todo-list__add-wrapper');
-const newTaskFieldset = newTaskContainer.querySelector('.todo-list__fieldset');
-const newTaskInput = newTaskContainer.querySelector('.todo-list__add-input');
-const newTaskAddButton = newTaskContainer.querySelector('.todo-list__add-btn');
-const newTaskTemplate = document.querySelector('#new-task').content.querySelector('.task');
+const pageBody = document.querySelector('.body');
+const todoList = pageBody.querySelector('.todo-list__list');
+const newTaskForm = pageBody.querySelector('.todo-list__add-wrapper');
+const newTaskFieldset = newTaskForm.querySelector('.todo-list__fieldset');
+const newTaskInput = newTaskForm.querySelector('.todo-list__add-input');
+const newTaskAddButton = newTaskForm.querySelector('.todo-list__add-btn');
+const newTaskTemplate = pageBody.querySelector('#new-task').content.querySelector('.task');
+const themeSelect = pageBody.querySelector('.theme-input');
 
 // Добавление задачи
 const addTask = () => {
@@ -18,21 +20,30 @@ const addTask = () => {
 	newTaskTextContainer.textContent = newTaskText;
 	todoList.appendChild(newTask);
 	newTaskInput.value = '';
+	// скрываем поле ввода и изменяем кнопку
+	newTaskAddButton.classList.remove('todo-list__add-btn--add');
+	newTaskFieldset.classList.remove('todo-list__fieldset--shown');
+
 };
 const onNewTaskAddButtonClick = (evt) => {
 	evt.preventDefault();
+	// Проверяем, открыто ли поле ввода задачи
 	const isTaskInputShown = newTaskFieldset.classList.contains('todo-list__fieldset--shown');
 	if (isTaskInputShown) {
 		//добавляем новую задачу в список
 		addTask();
-		newTaskAddButton.classList.remove('todo-list__add-btn--add');
-		newTaskFieldset.classList.remove('todo-list__fieldset--shown');
 	} else {
 		//показываем инпут для задачи
 		newTaskAddButton.classList.add('todo-list__add-btn--add');
 		newTaskFieldset.classList.add('todo-list__fieldset--shown');
 	}
 };
+const onNewTaskFormSubmit = (evt) => {
+	evt.preventDefault();
+	addTask();
+	newTaskAddButton.classList.remove('todo-list__add-btn--add');
+	newTaskFieldset.classList.remove('todo-list__fieldset--shown');
+}
 
 // Удаление задач
 const onTaskHandlerClick = (evt) => {
@@ -42,8 +53,25 @@ const onTaskHandlerClick = (evt) => {
 		return false;
 	}
 	currentTask.classList.add('task--hide');
+	currentTask.classList.add('task--transition');
 	setTimeout(() => currentTask.remove(), 800);
 };
 
+// Переключение тем
+const setTransition = () => {
+	pageBody.classList.add('body--transition');
+	setTimeout(() => {
+		pageBody.classList.remove('body--transition');
+	}, 1000);
+};
+
+const onThemeSelectChange = () => {
+	setTransition();
+	pageBody.classList.toggle('body--dark');
+};
+
+// Запускаем слушателей событий
 todoList.addEventListener('click', onTaskHandlerClick);
+newTaskForm.addEventListener('submit', onNewTaskFormSubmit);
 newTaskAddButton.addEventListener('click', onNewTaskAddButtonClick);
+themeSelect.addEventListener('change', onThemeSelectChange);
